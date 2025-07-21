b31ec7e1bde976847fdf05c17a1faf56650c8bd0
tanmayMt <tanmoy587d@gmail.com>
2025-07-21 16:24:08 +0530

    Loading State

---
+++ my-next-app1/app/about/page.js
@@ -0,0 +1,25 @@
+import React from 'react'
+
+export default function About(){
+  return (
+    <div>Lorem Ipsum - All the facts - Lipsum generator
+What is Lorem Ipsum? Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+All images
+See more images of Lorem ipsum
+Napo in: Schluss mit Lärm. So wird das Gehört geschützt
+08:03
+Napo in: Schluss mit Lärm. So wird das Gehört geschützt
+YouTubeSuva Schweiz167.3K viewsSep 1, 2015
+What is lorem Ipum filling text?
+The Lorem ipum filling text is used by graphic designers, programmers and printers with the aim of occupying the spaces of a website, an advertising product or an editorial production whose final text is not yet ready. This expedient serves to get an idea of the finished product that will soon be printed or disseminated via digital channels.
+loremipzum.com
+Dummy Text Generator Lorem Ipsum
+Lorem ipsum is a dummy text without any sense. It is a sequence of Latin words that, as they are positioned, do not form sentences with a complete sense, but give life to a test text useful to fill space…
+Global Web Icon
+loremipzum.com
+Dummy Text Generator | Lorem ipsum for webdesigners
+A handy Lorem Ipsum Generator that helps to create dummy text for all layout needs.
+Global Web Icon
+blindtextgenerator.com</div>
+  )
+}
---
+++ my-next-app1/app/about/vission/page.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function Vission(){
+  return (
+    <div>This is Vission Page</div>
+  )
+}
