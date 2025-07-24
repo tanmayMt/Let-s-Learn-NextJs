a66c119af69a0b13823bf862402e6ec59f07c0fb
tanmayMt <tanmoy587d@gmail.com>
2025-07-24 09:06:12 +0530

    make server compont to client component because of adding a button

--- my-next-app1/app/about/mission/page.js
+++ my-next-app1/app/about/mission/page.js
@@ -1,7 +1,25 @@
+"use client"
+
+
 import React from 'react'
 
-export default function About(){
+export default function Mission(){
   return (
-    <div>This is Mission Page</div>
+   <main className='mt-10'>
+    <h2><u>This is Mission Page</u></h2>
+    <p>
+      This is Mission PageThis is Mission PageThis is Mission PageThis is
+      Mission PageThis is Mission This is Mission PageThis is Mission Page
+      PageThis is Mission PageThis is Mission Page
+    </p>
+        
+      {/* For this button server component is now to applicatable it become Clien componet */}
+      <div className='mt-5'>
+      <button className='bg-indigo-700 rounded-sm px-4 py1' onClick={()=> console.log("I have Clicked Here")}>
+        Click Here
+      </button>
+    </div>
+
+   </main> 
   )
 }
--- my-next-app1/app/blogs/[id]/page.js
+++ my-next-app1/app/blogs/[id]/page.js
@@ -1,15 +1,16 @@
 import { notFound } from 'next/navigation';
 import React from 'react'
 
-const BlogPage = ({params}) => {
-const {id} = params;
 
-if(id >= "3"){
+const BlogPage = async ({ params }) => {
+  const { id } = await params;
+
+  if (id >= "3") {
     notFound(); // This will trigger a 404 page if the id is 3
-}
+  }
   return (
     <div>The Blog ID is :{id}</div>
-  )
+  );
 }
 
 export default BlogPage;
