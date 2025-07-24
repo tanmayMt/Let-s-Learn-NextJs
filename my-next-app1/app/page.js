3b58fa7a40641f48ed27c92216afac6d32cf4667
tanmayMt <tanmoy587d@gmail.com>
2025-07-24 10:55:29 +0530

    Image Optimization - <Image> 'next/image'

--- my-next-app1/app/page.js
+++ my-next-app1/app/page.js
@@ -1,13 +1,15 @@
 
 import Image from "next/image";
 import Profile from "@/public/images/Profile.jpeg";
+import IconT from "@/public/images/faviconT.png";
 
 export default function Home() {
   // console.log("Home Page");
   return (
     <main className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       This is Home Page
-      <Image placeholder="blur" src={Profile} alt="Profile" alt="Profile image"/>
+      <Image placeholder="blur" src={Profile} alt="Profile image"/><br></br>
+      <Image placeholder="blur" src={IconT} alt="IconT image"/>
     </main>
   );
 }
