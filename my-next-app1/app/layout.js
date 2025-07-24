b3ab4791bde21186525b8c8ca147fb1721e1d901
tanmayMt <tanmoy587d@gmail.com>
2025-07-24 11:11:01 +0530

    font  Optimization in layout.js

--- my-next-app1/app/layout.js
+++ my-next-app1/app/layout.js
@@ -1,16 +1,18 @@
-import { Geist, Geist_Mono } from "next/font/google";
+// import { Geist, Geist_Mono } from "next/font/google";
+import { Inter } from "next/font/google";   
 import "./globals.css";
 import Link from "next/link";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
+// const geistSans = Geist({
+//   variable: "--font-geist-sans",
+//   subsets: ["latin"],
+// });
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+// const geistMono = Geist_Mono({
+//   variable: "--font-geist-mono",
+//   subsets: ["latin"],
+// });
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
   title: "Create Next App",
@@ -21,7 +23,8 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en" className="p-6">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        // className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={inter.className}
       >
         <nav>
           <ul className="flex gap-6">
--- my-next-app1/app/page.js
+++ my-next-app1/app/page.js
@@ -7,7 +7,13 @@ export default function Home() {
   // console.log("Home Page");
   return (
     <main className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      This is Home Page
+      <h1>This is Home Page</h1>
+      <p>
+         To be part of an organization where I get a      
+         chance to use my knowledge and skills to  
+         contribute in the progress of the organization as   
+         well as myself. 
+      </p><br/>
       <Image placeholder="blur" src={Profile} alt="Profile image"/><br></br>
       <Image placeholder="blur" src={IconT} alt="IconT image"/>
     </main>
